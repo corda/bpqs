3421e1240b9e883f689de2ba68866887a3687339
igor nitto <nitto.igor@gmail.com>
2018-04-16 12:23:37 +0100

    Updated benchmarking script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 ./gradlew jar
 mvn install:install-file -Dfile=./build/libs/corda-pq-signature-1.0-SNAPSHOT.jar -DgroupId=net.corda.research.pq -DartifactId=corda-pq-signature -Dversion=1.0
+(cd benchmark && mvn clean install && java -jar ./target/benchmarks.jar)
+
